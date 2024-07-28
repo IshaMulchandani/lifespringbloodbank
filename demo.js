e998c7a1f4f6e9d79743ee3ee397ece09261b2f1
Isha Mulchandani <ishamulani1@gmail.com>
2024-07-28 15:54:12 +0530

    initial commit

---
+++ demo.js
@@ -0,0 +1,42 @@
+function mem1animate(){
+    img=document.getElementById('person1img')
+    nam=document.getElementById('person1name')
+    img.style.width='30vw'
+    img.style.height='45vh'
+    nam.style.fontSize='5vh'
+}
+function mem1deanimate(){
+    img=document.getElementById('person1img')
+    nam=document.getElementById('person1name')
+    img.style.width='20vw'
+    img.style.height='30vh'
+    nam.style.fontSize='2.5vh'
+}
+function mem2animate(){
+    img=document.getElementById('person2img')
+    nam=document.getElementById('person2name')
+    img.style.width='30vw'
+    img.style.height='45vh'
+    nam.style.fontSize='5vh'
+}
+function mem2deanimate(){
+    img=document.getElementById('person2img')
+    nam=document.getElementById('person2name')
+    img.style.width='20vw'
+    img.style.height='30vh'
+    nam.style.fontSize='2.5vh'
+}
+function mem3animate(){
+    img=document.getElementById('person3img')
+    nam=document.getElementById('person3name')
+    img.style.width='30vw'
+    img.style.height='45vh'
+    nam.style.fontSize='5vh'
+}
+function mem3deanimate(){
+    img=document.getElementById('person3img')
+    nam=document.getElementById('person3name')
+    img.style.width='20vw'
+    img.style.height='30vh'
+    nam.style.fontSize='2.5vh'
+}
